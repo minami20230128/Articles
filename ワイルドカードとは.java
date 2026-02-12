f71abfa0e19a8366cce784dfce27493cd8118844
minami <south614fruit@yahoo.co.jp>
2026-02-13 01:10:14 +0900

    上限境界ワイルドカードと下限境界ワイルドカードのコードが消えたので追加

--- ワイルドカードとは.java
+++ ワイルドカードとは.java
@@ -3,41 +3,103 @@
 // 非境界ワイルドカード
 import java.util.ArrayList;
 
-class Parent {
-    String name;
-    Parent(String name) {
-        this.name = name;
-    }
-}
-
-class Child extends Parent {
-    String name;
-    Child(String name) {
-        super(name);
-        this.name = name;
-    }
-}
-
-class GrandChild extends Child {
-    String name;
-    GrandChild(String name) {
-        super(name);
-        this.name = name;
-    }
-}
-
 public class Main {
     public static void main(String[] args) throws Exception {
-        ArrayList<? super Child> list = new ArrayList<>();
+        ArrayList<?> list = new ArrayList<>();
 
-        ArrayList<Parent> childList = new ArrayList<>(asList(new Child("AA"), new Parent("BB")));
+        ArrayList<String> strList = new ArrayList<>(asList("AA"));
         
-        list = childList;
-
-        Child c = list.get(0); // 返り値はObjectでしか扱えない
-        Object o = list.get(0);
+        list = strList;
+        String s = list.get(0); // エラー
+        Object s = list.get(0); // 通る。静的にはObject型としてしか使えない
         
-        list.add(null);
-        list.add(new Child("CC")); // 任意の型の引数を渡せる
+        list.add(null); // nullは渡せる
+        list.add("BB"); // これはNG
     }
 }
+
+// 上限境界ワイルドカード
+//import java.util.ArrayList;
+//import static java.util.Arrays.asList;
+//
+//class Parent {
+//    String name;
+//    Parent(String name) {
+//        this.name = name;
+//    }
+//}
+//
+//class Child extends Parent {
+//    String name;
+//    Child(String name) {
+//        super(name);
+//        this.name = name;
+//    }
+//}
+//
+//class GrandChild extends Child {
+//    String name;
+//    GrandChild(String name) {
+//        super(name);
+//        this.name = name;
+//    }
+//}
+//
+//public class Main {
+//    public static void main(String[] args) throws Exception {
+//        ArrayList<? extends Child> list = new ArrayList<>();
+//
+//        ArrayList<Child> childList = new ArrayList<>(asList(new Child("AA"), new GrandChild("BB")));
+//        
+//        list = childList;
+//
+//        Child c = list.get(0); // 返り値を任意の型で受け取れる
+//        Object o = list.get(0);
+//        
+//        list.add(null);
+//        list.add(new Child("CC")); // 引数はnullしか渡せない
+//    }
+//}
+//
+//// 下限境界ワイルドカード
+//import java.util.ArrayList;
+//import static java.util.Arrays.asList;
+//
+//class Parent {
+//    String name;
+//    Parent(String name) {
+//        this.name = name;
+//    }
+//}
+//
+//class Child extends Parent {
+//    String name;
+//    Child(String name) {
+//        super(name);
+//        this.name = name;
+//    }
+//}
+//
+//class GrandChild extends Child {
+//    String name;
+//    GrandChild(String name) {
+//        super(name);
+//        this.name = name;
+//    }
+//}
+//
+//public class Main {
+//    public static void main(String[] args) throws Exception {
+//        ArrayList<? super Child> list = new ArrayList<>();
+//
+//        ArrayList<Parent> childList = new ArrayList<>(asList(new Child("AA"), new Parent("BB")));
+//        
+//        list = childList;
+//
+//        Child c = list.get(0); // 返り値はObjectでしか扱えない
+//        Object o = list.get(0);
+//        
+//        list.add(null);
+//        list.add(new Child("CC")); // 任意の型の引数を渡せる
+//    }
+//}
